cc5b9c214b1bc5370deae6af3424721b90fdcbc2
Dinesh <dineshchander1995@gmail.com>
2021-05-21 19:23:33 +0530

    Update RestController.java

--- src/main/java/com/happy/workplace/controller/RestController.java
+++ src/main/java/com/happy/workplace/controller/RestController.java
@@ -3,6 +3,7 @@ package com.happy.workplace.controller;
 import java.util.Base64;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
